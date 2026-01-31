eb3215125ded523ae32bed8a6fc557ad9c120628
Taras Karpuk <taras.karpuk@gmail.com>
2026-01-31 10:48:06 -0700

    add IRepository interface

---
+++ ProductCatalog/Data/IRepository.cs
@@ -0,0 +1,12 @@
+﻿namespace ProductCatalog.Data
+{
+    public interface IRepository<T>
+    {
+        Task<List<T>> GetAllAsync();
+        Task<T> GetByIdAsync(int id);
+        Task<int> CreateAsync(T item);
+        Task UpdateAsync(T item);
+        Task DeleteAsync(int id);
+        Task<bool> ExistsAsync(int id);
+    }
+}
